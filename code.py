31bd1b4db95ec7bdea689ac4daa5ed7e901a2ad6
heygourab <gourabblg901@gmail.com>
2021-12-07 14:04:25 +0530

    Multithreading

--- code.py
+++ code.py
@@ -1,28 +1,37 @@
 import time, threading
-# threading using for use multitasking....
-# using 2 function at a same time.
+
+# Multithreading is a technique where multiple are spawned
+# by a process to do different tasks, at about the same time,
+# just one after the other, This gives you the illusion that
+# running in parallel, but they are actually run in a
+# concurrent manner. In Python, the Global Interpreter Lock
+# prevents the threads form running simultaneously.
+
+
 def calc_cube(numbers):
     for i in numbers:
         time.sleep(0.1)
-        print('cube:', i * i)
+        print('cube:', i**3)
 
 
 def calc_square(numbers):
     for i in numbers:
         time.sleep(0.1)
-        print('square:', i * i * i)
+        print('square:', i**2)
 
 
-arr = [2, 3, 8, 9]
-t = time.time()
+if __name__ == '__main__':
 
-t1 = threading.Thread(target=calc_cube, args=(arr, ))
-t2 = threading.Thread(target=calc_square, args=(arr, ))
+    arr = [2, 3, 8, 9]
+    t = time.time()
 
-t1.start()
-t2.start()
+    t1 = threading.Thread(target=calc_cube, args=(arr, ))
+    t2 = threading.Thread(target=calc_square, args=(arr, ))
 
-t1.join()
-t2.join()
+    t1.start()
+    t2.start()
 
-print('done in:', time.time() - t)
+    t1.join()
+    t2.join()
+
+    print('done in:', time.time() - t)
