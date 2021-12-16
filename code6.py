9045cceda05427da9d46c3664e934f99a5009316
heygourab <gourabblg901@gmail.com>
2021-12-17 01:42:08 +0530

    idk

--- code6.py
+++ code6.py
@@ -1,1 +1,17 @@
-# 😁  don't use pool 
+import time
+from multiprocessing import Pool
+
+
+def f(x):
+    return x * x
+
+
+if __name__ == '__main__':
+    t = time.time()
+    with Pool(5) as p:
+        print(p.map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+    print('total time:', time.time() - t)
+    print('without Pool----->')
+    print(list(map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])))
+    print('total time:', time.time() - t)
